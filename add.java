299443c489e26719f563cb6c28cdf574618fd250
tanmayithape <96616122+tanmayithape@users.noreply.github.com>
2026-08-04 12:24:12 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,24 @@
+package addition;
+
+public class add {
+	public static void main (String[] args) {
+        int a=13;
+        int b=12;
+        int c;
+        int d;
+        int e;
+        int f;
+        
+        c=a+b;
+        d=a-b;
+        e=a/b;
+        f=a*b;
+
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        System.out.println("e = "+e);
+        System.out.println("f = "+f);
+
+    }
+
+}
