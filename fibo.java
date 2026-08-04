3bc422aef07ce7b7032962bd7f01906c06c7744a
tanmayithape <96616122+tanmayithape@users.noreply.github.com>
2026-08-04 12:14:12 +0530

    Add files via upload
    Fibonaccii series

---
+++ fibo.java
@@ -0,0 +1,16 @@
+package factorial;
+
+public class fibo {
+	public static void main(String[]strgs) {
+  	  int sum=1;
+  	  int n=5;
+  	  
+  	  for(int i=1;i<=n;i++) {
+  		  sum=sum*i;
+  		  
+  		  System.out.println(sum);
+  	  }
+  	  
+    }
+  
+}
