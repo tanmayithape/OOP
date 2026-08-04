12b01eac57bcb9f158cc88f242abffe7984f8ffd
tanmayithape <96616122+tanmayithape@users.noreply.github.com>
2026-08-04 12:20:23 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,21 @@
+package swaping;
+
+public class swap {
+	 public static void main(String[] args) {
+		int a=11;
+		int b=12;
+		int temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("Swaping of two number :");
+		System.out.println("---------------------");
+		System.out.println(a);
+		System.out.println("---------------------");
+		System.out.print(b);
+			
+	}
+	}
+
+
+
